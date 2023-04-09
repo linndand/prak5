becb2e772d38541a34b034eb60968af04f7be9e3
linndand <linnda.p.n@students.amikom.ac.id>
2023-04-09 10:37:27 +0700

    prak5

---
+++ prak5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            numbers.CariNilaiMin(20, 21);
+            numbers.CariNilaiMin(100, 20, 11);
+            numbers.CariNilaiMax(20, 21);
+            numbers.CariNilaiMax(100, 20, 11);
+        }
+    }
+}
